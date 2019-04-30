7ecf292563dcb50f5705f0263ff274157a553ac6
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2019-04-30 23:40:28 +0900

    fix: twtter以外のページを閲覧したときにエラーになっていた問題に対処
    executeScript 権限は "https://twitter.com/*" にしか無いが、全ページで実行していたため
    url をチェックし https://twitter.com/* でのみ実行
    
    ※ 権限が無いため https://twitter.com/* 以外では tab.url は undefined となる

--- js/background.js
+++ js/background.js
@@ -1,6 +1,6 @@
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-	if (changeInfo.status === 'complete') {
+	if (changeInfo.status === 'complete' && tab.url && tab.url.startsWith("https://twitter.com/")) {
 		chrome.tabs.executeScript(tab.id, { file: "js/base.js" })
 	}
 })
